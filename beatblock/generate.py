c8b93f6e32c9cffb354a37fd35bec710434cad24
Bentroen <bemcdc@gmail.com>
2022-08-09 06:09:35 -0300

    Add basic block placement

---
+++ beatblock/generate.py
@@ -0,0 +1,23 @@
+from . import map
+
+from beet import Context, Function, FunctionTag
+
+
+def beet_default(ctx: Context) -> None:
+
+    # Set up tick and load functions
+    ctx.data["minecraft:load"] = FunctionTag({"values": ["beatblock:load"]})
+    #ctx.data["minecraft:tick"] = FunctionTag({"values": ["beatblock:tick"]})
+
+
+    load_function = Function()
+
+    #function.append("fill 0 ")
+    load_function.append("say hi")
+
+    for block in map.SOLID:
+        load_function.append("setblock {} {} {} minecraft:purpur_block".format(*block))
+
+    ctx.data["beatblock:load"] = load_function
+
+
---
+++ beatblock/map.py
@@ -0,0 +1,13 @@
+# This module stores the positions for all blocks in the Beat Block Galaxy.
+
+SOLID = [
+    (0, 0, 0),
+    (2, 0, 0),
+    (0, 0, 2),
+    (2, 0, 2),
+]
+
+BEAT_GREEN = []
+
+
+BEAT_YELLOW = []
