b9c6486297f398a5e170509f015428f08a49226b
Bentroen <bemcdc@gmail.com>
2022-08-10 03:37:14 -0300

    Invert x and z placement coordinates
    Facing south has positive Z in front of you, but *negative* X to your right. That causes coordinates to be flipped, which is such a mess. Better to flip X and Z and do stuff facing east (+X with +Z to your right) instead.

--- beatblock/generate.py
+++ beatblock/generate.py
@@ -19,7 +19,18 @@ def beet_default(ctx: Context) -> None:
     load_function.append("fill -8 -61 -8 24 -61 24 air")
     load_function.append("say hi")
 
-    for block in map.SOLID:
-        load_function.append("setblock {} {} {} minecraft:purpur_block".format(*block))
+    swizzle = lambda x: x[::-1]  # invert Z and X coordinates
+    for block in map_data.SOLID:
+        load_function.append(
+            "setblock {} {} {} minecraft:purpur_block".format(*swizzle(block))
+        )
+    for block in map_data.BEAT_GREEN:
+        load_function.append(
+            "setblock {} {} {} minecraft:emerald_block".format(*swizzle(block))
+        )
+    for block in map_data.BEAT_YELLOW:
+        load_function.append(
+            "setblock {} {} {} minecraft:gold_block".format(*swizzle(block))
+        )
 
     ctx.data["beatblock:load"] = load_function
