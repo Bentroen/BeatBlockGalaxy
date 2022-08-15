fe8c1fd75f1631fa5dca61ef4ac7ececc33b7aaa
Bentroen <bemcdc@gmail.com>
2022-08-15 05:40:13 -0300

    Patch song to use instrument file instead of name as sound event
    Unlike the data pack export in NBS, pigstep uses the instrument sound file (rather than the instrument name) to decide the sound event to use in `/playsound` . This is the most logical way of doing it, but the file name field is not as easily controllable from inside NBS.

--- beatblock/music.py
+++ beatblock/music.py
@@ -1,12 +1,46 @@
+from io import BytesIO
+import os
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 from beet import Context
 from pigstep import pigstep
+
+import pynbs
+
+
+def write_song_to_open_file(song: pynbs.File, buff: BytesIO):
+    pynbs.Writer(buff).encode_file(song, pynbs.CURRENT_NBS_VERSION)
 
 
 def beet_default(ctx: Context) -> None:
 
+    song = pynbs.read(Path("songs", "beatblock.nbs"))
+
+    # Set instrument name to sound event name (pigstep uses the sound file field as the sound event)
+    for ins in song.instruments:
+        new_ins = pynbs.Instrument(
+            id=ins.id,
+            name=ins.name,
+            file=ins.name,
+            pitch=ins.pitch,
+            press_key=ins.press_key,
+        )
+        song.instruments[ins.id] = new_ins
+
+    temp_songs_path = Path("songs", ".temp")
+    if not os.path.exists(temp_songs_path):
+        os.makedirs(temp_songs_path)
+
+    filename = Path(temp_songs_path, "beatblock.nbs")
+    song.save(filename)
+
+    # tempfile = NamedTemporaryFile()
+    # filename = os.path.basename(tempfile.name)
+    # write_song_to_open_file(song, tempfile)
+
     ctx.require(
         pigstep(
-            load=["songs/beatblock.nbs"],
+            load=[str(filename)],
             source="record",
             templates={
                 "play": "start.mcfunction",
